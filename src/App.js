433c43f2bca1a194afdd27e680ea04a3af6e3064
Uditi Arora <uditiarora@gmail.com>
2020-10-11 12:46:55 +0530

    converting to react-redux app

--- src/App.js
+++ src/App.js
@@ -1,34 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
-import logo from './logo.svg';
 import './App.css';
 
 import { simpleAction } from './actions/simpleAction'
 
-/* 
- * mapDispatchToProps
-*/
 const mapDispatchToProps = dispatch => ({
   simpleAction: () => dispatch(simpleAction())
 })
 
-/* 
- * mapStateToProps
-*/
+
 const mapStateToProps = state => ({
   ...state
 })
 
-/**
- * @class App
- * @extends {Component}
- */
 class App extends Component {
-  /**
-   * @memberof App
-   * @summary handles button click 
-   */
+  
   simpleAction = (event) => {
     this.props.simpleAction();
   }
